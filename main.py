21907306ba31f63e74a591b09e2a05a358b9d8f3
jeremyALBERTINI <jeremy.albertini@epitech.eu>
2024-11-20 15:23:06 +0100

    first

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from matrix import Matrix
 
-matrix = Matrix()
+matrix = Matrix(6)
 
 matrix.display()
-vec = matrix.get_rnd_empty_case()
-print()
-matrix.matrix[vec.x][vec.y] = 2
-matrix.display()
+
+
