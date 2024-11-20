7b0324b694ccacd981c61db689e2e4171ef55002
jeremyALBERTINI <jeremy.albertini@epitech.eu>
2024-11-20 16:40:35 +0100

    add mouve_up

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from matrix import Matrix
 
 matrix = Matrix(6)
+matrix.matrix[4][3] = 2
+matrix.matrix[3][3] = 2
+matrix.matrix[5][3] = 4
+matrix.matrix[4][2] = 2
 
 matrix.display()
+print("\n")
+matrix.mouv_up()
+matrix.display()
+
 
 
