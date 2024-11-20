0f554e155bf2132f1ea52bd049d69e81954ab8b6
Florian Demartini <florian.demartini@epitech.eu>
2024-11-20 15:56:20 +0100

    main loop

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from matrix import Matrix
+from player import Player
 
+def start_board(matrix):
+    vec = matrix.get_rnd_empty_case()
+    matrix.matrix[vec.x][vec.y] = 2
+    vec = matrix.get_rnd_empty_case()
+    matrix.matrix[vec.x][vec.y] = 2
+    matrix.display()
+
+def game_loop(matrix, player):
+    while not player.finish:
+        inp = input()
+        print()
+        matrix.move(inp)
+        vec = matrix.get_rnd_empty_case()
+        matrix.matrix[vec.x][vec.y] = 2
+        matrix.display()
+
+player = Player("Jeremy")
 matrix = Matrix()
 
-matrix.display()
-vec = matrix.get_rnd_empty_case()
-print()
-matrix.matrix[vec.x][vec.y] = 2
-matrix.display()
+start_board(matrix)
+game_loop(matrix, player)
