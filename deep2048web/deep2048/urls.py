8cbaf0a7e88a4c0723991d989bf8ba80f74d6565
jeremyALBERTINI <jeremy.albertini@epitech.eu>
2024-11-23 12:36:05 +0100

    cahnge size off map

--- deep2048web/deep2048/urls.py
+++ deep2048web/deep2048/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("update/", views.update_matrix, name="update_matrix"),
     path("reset/", views.reset_matrix, name="reset_matrix"),
+    path("update_rules/", views.update_rules, name="update_rules")
 ]
