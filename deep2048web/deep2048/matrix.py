b7ee51de644f739188a3ebff5f6d5ed7930600ba
Florian Demartini <florian.demartini@epitech.eu>
2025-01-30 17:11:26 +0100

    ev listener

--- deep2048web/deep2048/matrix.py
+++ deep2048web/deep2048/matrix.py
@@ -2,14 +2,27 @@ from random import randint
 from .vector import Vector
 from .player import Player, Model
 from copy import deepcopy
+from typing import List, Tuple
+
+class MoveRecord:
+    def __init__(self, value: int, from_pos: Tuple[int, int], to_pos: Tuple[int, int], merged: bool = False):
+        self.value = value
+        self.from_pos = from_pos
+        self.to_pos = to_pos
+        self.merged = merged
+
+    def __str__(self):
+        action = "merged into" if self.merged else "moved to"
+        return f"Block {self.value} {action} ({self.to_pos[0]}, {self.to_pos[1]}) from ({self.from_pos[0]}, {self.from_pos[1]})"
 
 class Matrix:
-    def __init__(self, name, model=Model.HUMAN, size=4, win = 1):
+    def __init__(self, name, model=Model.HUMAN, size=4, win=1):
         self.size = size
         self.matrix = []
         self.player = Player(name, model)
         self.win = win
         self.playing = False
+        self.move_history: List[MoveRecord] = []
         self.create()
 
     def create(self):
@@ -50,108 +63,220 @@ class Matrix:
         else:
             self.matrix[vec.x][vec.y] = nb
 
-    def go_up(self):
+    def go_up(self, human=False):
+        moves = [] if human else None
         for i in range(self.size):
             for j in range(self.size):
                 if self.matrix[i][j] != 0:
                     k = i
                     while k > 0 and self.matrix[k - 1][j] == 0:
+                        if human:
+                            moves.append(MoveRecord(
+                                self.matrix[k][j],
+                                (k, j),
+                                (k - 1, j)
+                            ))
                         temp = self.matrix[k][j]
                         self.matrix[k][j] = self.matrix[k - 1][j]
                         self.matrix[k - 1][j] = temp
                         k -= 1
+        return moves
 
-    def merge_up(self):
+    def merge_up(self, human=False):
+        moves = [] if human else None
         for i in range(1, self.size):
             for j in range(self.size):
                 if self.matrix[i][j] == self.matrix[i - 1][j] and i > 0:
+                    if human:
+                        moves.append(MoveRecord(
+                            self.matrix[i][j],
+                            (i, j),
+                            (i - 1, j),
+                            merged=True
+                        ))
                     self.matrix[i][j] = 0
                     self.matrix[i - 1][j] *= 2
                     self.player.score += self.matrix[i - 1][j]
+        return moves
 
-    def move_up(self):
+    def move_up(self, human=False):
         matrixBefore = deepcopy(self)
-        self.go_up()
-        self.merge_up()
-        self.go_up()
+        self.move_history = []  # Clear previous move history
+        
+        if human:
+            moves1 = self.go_up(human=True)
+            moves2 = self.merge_up(human=True)
+            moves3 = self.go_up(human=True)
+            self.move_history.extend(moves1)
+            self.move_history.extend(moves2)
+            self.move_history.extend(moves3)
+        else:
+            self.go_up()
+            self.merge_up()
+            self.go_up()
+            
         self.new_case(matrixBefore)
 
-    def go_down(self):
+    def go_down(self, human=False):
+        moves = [] if human else None
         for i in range(self.size - 1, -1, -1):
             for j in range(self.size - 1, -1 ,-1):
                 if self.matrix[i][j] != 0:
                     k = i
-                    while k < self.size -1 and self.matrix[k + 1][j] == 0:
+                    while k < self.size - 1 and self.matrix[k + 1][j] == 0:
+                        if human:
+                            moves.append(MoveRecord(
+                                self.matrix[k][j],
+                                (k, j),
+                                (k + 1, j)
+                            ))
                         temp = self.matrix[k][j]
                         self.matrix[k][j] = self.matrix[k + 1][j]
                         self.matrix[k + 1][j] = temp
                         k += 1
+        return moves
 
-    def merge_down(self):
+    def merge_down(self, human=False):
+        moves = [] if human else None
         for i in range(self.size - 2, -1, -1):
             for j in range(self.size - 1, -1 ,-1):
-                if self.matrix[i][j] == self.matrix[i + 1][j] and i < self.size -1:
+                if self.matrix[i][j] == self.matrix[i + 1][j] and i < self.size - 1:
+                    if human:
+                        moves.append(MoveRecord(
+                            self.matrix[i][j],
+                            (i, j),
+                            (i + 1, j),
+                            merged=True
+                        ))
                     self.matrix[i][j] = 0
                     self.matrix[i + 1][j] *= 2
                     self.player.score += self.matrix[i + 1][j]
+        return moves
 
-    def move_down(self):
+    def move_down(self, human=False):
         matrixBefore = deepcopy(self)
-        self.go_down()
-        self.merge_down()
-        self.go_down()
+        self.move_history = []  # Clear previous move history
+        
+        if human:
+            moves1 = self.go_down(human=True)
+            moves2 = self.merge_down(human=True)
+            moves3 = self.go_down(human=True)
+            self.move_history.extend(moves1)
+            self.move_history.extend(moves2)
+            self.move_history.extend(moves3)
+        else:
+            self.go_down()
+            self.merge_down()
+            self.go_down()
+            
         self.new_case(matrixBefore)
 
-    def go_left(self):
+    def go_left(self, human=False):
+        moves = [] if human else None
         for i in range(self.size):
             for j in range(self.size):
                 if self.matrix[i][j] != 0:
                     k = j
                     while k > 0 and self.matrix[i][k - 1] == 0:
+                        if human:
+                            moves.append(MoveRecord(
+                                self.matrix[i][k],
+                                (i, k),
+                                (i, k - 1)
+                            ))
                         temp = self.matrix[i][k]
                         self.matrix[i][k] = self.matrix[i][k - 1]
                         self.matrix[i][k - 1] = temp
                         k -= 1
+        return moves
 
-    def merge_left(self):
+    def merge_left(self, human=False):
+        moves = [] if human else None
         for i in range(self.size):
             for j in range(1, self.size):
                 if self.matrix[i][j] == self.matrix[i][j - 1] and j > 0:
+                    if human:
+                        moves.append(MoveRecord(
+                            self.matrix[i][j],
+                            (i, j),
+                            (i, j - 1),
+                            merged=True
+                        ))
                     self.matrix[i][j] = 0
                     self.matrix[i][j - 1] *= 2
                     self.player.score += self.matrix[i][j - 1]
+        return moves
 
-    def move_left(self):
+    def move_left(self, human=False):
         matrixBefore = deepcopy(self)
-        self.go_left()
-        self.merge_left()
-        self.go_left()
+        self.move_history = []  # Clear previous move history
+        
+        if human:
+            moves1 = self.go_left(human=True)
+            moves2 = self.merge_left(human=True)
+            moves3 = self.go_left(human=True)
+            self.move_history.extend(moves1)
+            self.move_history.extend(moves2)
+            self.move_history.extend(moves3)
+        else:
+            self.go_left()
+            self.merge_left()
+            self.go_left()
+            
         self.new_case(matrixBefore)
 
-    def go_right(self):
+    def go_right(self, human=False):
+        moves = [] if human else None
         for i in range(self.size - 1, -1, -1):
             for j in range(self.size - 1, -1 ,-1):
                 if self.matrix[i][j] != 0:
                     k = j
-                    while k < self.size -1 and self.matrix[i][k + 1] == 0:
+                    while k < self.size - 1 and self.matrix[i][k + 1] == 0:
+                        if human:
+                            moves.append(MoveRecord(
+                                self.matrix[i][k],
+                                (i, k),
+                                (i, k + 1)
+                            ))
                         temp = self.matrix[i][k]
                         self.matrix[i][k] = self.matrix[i][k + 1]
                         self.matrix[i][k + 1] = temp
                         k += 1
+        return moves
 
-    def merge_right(self):
+    def merge_right(self, human=False):
+        moves = [] if human else None
         for i in range(self.size - 1, -1, -1):
             for j in range(self.size - 2, -1 ,-1):
-                if self.matrix[i][j] == self.matrix[i][j + 1] and j < self.size -1:
+                if self.matrix[i][j] == self.matrix[i][j + 1] and j < self.size - 1:
+                    if human:
+                        moves.append(MoveRecord(
+                            self.matrix[i][j],
+                            (i, j),
+                            (i, j + 1),
+                            merged=True
+                        ))
                     self.matrix[i][j] = 0
                     self.matrix[i][j + 1] *= 2
                     self.player.score += self.matrix[i][j + 1]
+        return moves
 
-    def move_right(self):
+    def move_right(self, human=False):
         matrixBefore = deepcopy(self)
-        self.go_right()
-        self.merge_right()
-        self.go_right()
+        self.move_history = []  # Clear previous move history
+        
+        if human:
+            moves1 = self.go_right(human=True)
+            moves2 = self.merge_right(human=True)
+            moves3 = self.go_right(human=True)
+            self.move_history.extend(moves1)
+            self.move_history.extend(moves2)
+            self.move_history.extend(moves3)
+        else:
+            self.go_right()
+            self.merge_right()
+            self.go_right()
+            
         self.new_case(matrixBefore)
 
     def new_case(self, matrixBefore):
@@ -190,12 +315,23 @@ class Matrix:
         if nb == 3:
             self.move_right()
 
-    def move_inp(self, direction):
+    def move_inp(self, direction, human=False):
         if direction == "up":
-            self.move_up()
+            self.move_up(human)
         elif direction == "down":
-            self.move_down()
+            self.move_down(human)
         elif direction == "left":
-            self.move_left()
+            self.move_left(human)
         elif direction == "right":
-            self.move_right()
+            self.move_right(human)
+            
+        if human:
+            return self.move_history
+
+    def get_move_history(self):
+        return self.move_history
+
+    def print_move_history(self):
+        print("\nMove History:")
+        for move in self.move_history:
+            print(str(move))
